2abe8bb43b33af160c8309ba9c3a402c7eccc464
John Jacobs <Johnj28@gmail.com>
2020-07-26 18:28:35 +0200

    Metrisch_schroefdraad.py nu wel aangepast!

--- Metrisch_schroefdraad.py
+++ Metrisch_schroefdraad.py
@@ -1,10 +1,12 @@
 from math import pi
+from pcinput import getFloat, getInteger
 
 print("\n"*5)
 print("Voer voor metrisch schroefdraad de gegevens in.")
 print()
-diameter = int(input("Geef de diameter >"))
-spoed = float(input("Geef de spoed >"))
+
+diameter = getInteger("Geef de diameter >")
+spoed = getFloat( "Geef de spoed: " )
 
 d = diameter
 p = spoed
@@ -16,7 +18,7 @@ d2 = round(d-0.6495*p, 2)  # Flanken diameter
 d3 = round(d-1.2269*p, 2)  # Kern van moer
 D1 = round(d-1.0825*p, 2)  # Kern van Bout
 boor = d-p                 # Boor diameter
-hoek = 60
+hoek = 60                  # Tophoek
 doorsnede_volume = round((pi/4)*pow(((d2+d3)/2), 2), 0)
 
 
@@ -28,10 +30,10 @@ print("*** Schroefdraad M %3.0f x %2.2f      ***" % (d, p))
 print("***                                ***")
 print("*** Bout snede diepte h3= %8.2f ***" % (h3))
 print("*** Moer snede diepte H1= %8.2f ***" % (H1))
-print("*** Afronding R         = %8.2f ***" % (R))
-print("*** Flanken diameter d2 = %8.2f ***" % (d2))
-print("*** Kern van moer d3    = %8.2f ***" % (d3))
-print("*** Kern van bout D1    = %8.2f ***" % (D1))
+print("*** Afronding         R = %8.2f ***" % (R))
+print("*** Flanken diameter  d2= %8.2f ***" % (d2))
+print("*** Kern van moer     d3= %8.2f ***" % (d3))
+print("*** Kern van bout     D1= %8.2f ***" % (D1))
 print("*** Boor diameter       = %8.2f ***" % (boor))
 print("*** Doorsnede volume    = %8.2f ***" % (doorsnede_volume))
 print("**************************************")
